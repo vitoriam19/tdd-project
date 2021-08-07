c45bc2c6c1a7558a0dce0d15431cb4c25302b101
Vitória Mirella <vitoriamirellapn@gmail.com>
2021-08-06 22:36:25 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from lists.models import Item
 
 from lists.views import home_page
 
@@ -25,3 +26,21 @@ class HomePageTest(TestCase):
       self.assertIn('A new list item', response.content.decode())
       self.assertTemplateUsed(response, 'home.html')
 
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(second_saved_item.text, 'O segundo item')
