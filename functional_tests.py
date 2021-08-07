d3c4473647945e0110a126fcd022eefc09114af2
Vitória Mirella <vitoriamirellapn@gmail.com>
2021-08-06 22:25:28 -0300

    Refatorando o Teste Funcional

--- functional_tests.py
+++ functional_tests.py
@@ -50,12 +50,35 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
         self.assertIn('2: Estudar testes de unidade', [row.text for row in rows])
 
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.check_for_row_in_list_table('2: Estudar testes de unidade')
+
         # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
         # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
 
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
+
+    def check_for_row_in_list_table(self, row_text):
+      table = self.browser.find_element_by_id('id_list_table')
+      rows = table.find_elements_by_tag_name('tr')
+      self.assertIn(row_text, [row.text for row in rows])
+
 
 if __name__ == '__main__':
     unittest.main()
