24e649a2a0318c80814a7e6d056f454aa517a6d0
Vitória Mirella <vitoriamirellapn@gmail.com>
2021-08-09 17:51:03 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-from lists.models import Item
+from django.test import TestCase
+from lists.models import Item, List
 
 from lists.views import home_page
 
@@ -21,16 +22,24 @@ class HomePageTest(TestCase):
 
       self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
         first_item = Item()
         first_item.text = 'O primeiro item'
+        first_item.list = my_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'O segundo item'
+        second_item.list = my_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -38,7 +47,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
         self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, my_list)
 
 class ListViewTest(TestCase):
 
